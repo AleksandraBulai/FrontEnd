42c54fd44eda52a113238e311de7a351ec890254
AleksandraBulai <alexandra.bulai2012@gmail.com>
2024-02-01 19:07:40 +0100

    Commit_One

---
+++ src/components/CommentsContainer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Comment from '../Comment'
+
+export default function CommentsContainer({ comments, }) {
+  return (
+    <div>
+
+    {
+      comments.map( el => <Comment key ={el.id} {...el} />)
+    }
+    </div>
+  )
+}
